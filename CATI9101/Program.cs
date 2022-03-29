a593a09bb5d7a42ce7664ebba8cd008ec4d36f75
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2022-03-29 08:15:47 -0300

    Hello Well bom dia

--- CATI9101/Program.cs
+++ CATI9101/Program.cs
@@ -6,6 +6,11 @@ namespace CATI9101
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Digite seu nome");
+            string nome = Console.ReadLine();
+            Console.WriteLine("Bom dia " + nome + "! Seja bem vindo");
+            Console.ReadKey();
         }
+
     }
 }
