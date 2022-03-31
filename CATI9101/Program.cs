1fc094efd519958baed02dfbd61346315f24d0ec
WELLINGTON VIEIRA DOS SANTOS <wellington.vsantos@gmail.com>
2022-03-31 11:41:04 -0300

    manipulação de matrizes

--- CATI9101/Program.cs
+++ CATI9101/Program.cs
@@ -140,7 +140,6 @@ namespace CATI9101
                 jogadores[i] = Console.ReadLine();
             }
 
-*/
             //Minipulação de Strings
             string nome = "  Wellington Vieira dos santos  ";
             Console.WriteLine(nome + " - " + nome.Length);
@@ -189,8 +188,35 @@ namespace CATI9101
             Console.WriteLine(fraseinv);
             Console.WriteLine();
 
-
-
+*/
+            // Manipulação de Matrizes
+            Console.WriteLine();
+            int[] numeros = new int[6];
+            Random random = new Random();
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                int sorteado = random.Next(1,60);
+                if (i==0)
+                    numeros[i] = sorteado;
+                else // a partir do segundo número sorteado
+                {
+                   if (Array.BinarySearch(numeros, sorteado) < 0)
+                        numeros[i] = sorteado;
+                }
+                if (i != numeros.Length - 1)
+                    Console.Write(numeros[i] + "-");
+                else
+                    Console.Write(numeros[i]);
+            }
+            Console.WriteLine();
+            Array.Sort(numeros);// ordena os números no vetor
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                if (i != numeros.Length - 1)
+                    Console.Write(numeros[i] + "-");
+                else
+                    Console.Write(numeros[i]);
+            }
 
             Console.ReadKey(); 
         }
